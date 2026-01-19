c6395546a957a25c7ae73a4263a27fd28621ec10
Antony Peña <antonypenamejia2705@gmail.com>
2026-01-19 18:19:00 -0500

    Cambiando swagger configuration

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -4,7 +4,7 @@ export const swaggerOptions = {
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'API Ecommerce - Users',
+      title: 'Ecommerce',
       version: '1.0.0',
       description: 'Documentación del módulo Users'
     },
